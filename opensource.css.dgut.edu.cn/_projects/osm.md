250c37cea31ed4485a3f9c97ab615c8ec8c3e533
150149 <1802796278@qq.com>
2021-03-13 17:54:57 +0800

    2021.3.13

---
+++ opensource.css.dgut.edu.cn/_projects/osm.md
@@ -0,0 +1,6 @@
+---
+title: Open Service Mesh
+description: Open Service Mesh (OSM) is a lightweight and extensible cloud native service mesh
+linkText: About this project
+projectUrl: https://openservicemesh.io/
+---
---
+++ opensource.css.dgut.edu.cn/build-image.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+docker build -t css-opensource:latest-alpine .
+#docker tag sai-openjdk:latest-alpine registry.cn-shenzhen.aliyuncs.com/douya-app/sai-openjdk
+#docker push registry.cn-shenzhen.aliyuncs.com/douya-app/sai-openjdk
